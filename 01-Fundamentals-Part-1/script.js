ac95420bdec0f260f793db7832db3d30beee7680
sgrazys <s.grazys@gmail.com>
2023-02-17 21:30:53 +0200

    init commit

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,211 @@
+/*
+
+let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jokubas';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jokubas_tauras = 'JT';
+let $new = 27;
+
+let person = 'Marija';
+
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Tauras');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const bithYear = 1991;
+// bithYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Grazys';
+console.log(lastName);
+
+// Math operators
+const now = 2037;
+const ageSaulius = now - 1991;
+const ageMarija = now - 2018;
+console.log(ageSaulius, ageMarija);
+
+console.log(ageSaulius * 2, ageSaulius / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Tauras';
+const lastName = 'Grazys';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x -1
+x--; // x = x -1
+console.log(x);
+
+// Comparison operators
+console.log(ageSaulius > ageMarija); // >, <, >=, <=
+console.log(ageMarija >= 18);
+
+const isFullAge = ageMarija >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+const now = 2037;
+const ageSaulius = now - 1991;
+const ageMarija = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageSaulius + ageMarija) / 2;
+console.log(ageSaulius, ageMarija, averageAge);
+*/
+
+/* const firstName = 'Tauras';
+const job = 'student';
+const bithYear = 1998;
+const year = 2037
+
+const tauras = "I'm " + firstName + ', a ' + (year - bithYear) + ' year old ' + job + '!';
+console.log(tauras);
+
+const taurasNew = `I'm ${firstName}, a ${year - bithYear} years old ${job}!`;
+console.log(taurasNew);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String with
+multiple
+lines`);
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving licnse 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young, wait another ${yearsLeft} years.`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
+*/
+
+/* //  type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Tauras'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n); */
+
+// Five falsy values: 0, '', undefined, null, NaN
+
+/* console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Tauras'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log(`Don't spend it all ;)`);
+} else {
+    console.log('You should get a job!');
+}
+
+let height = 0;
+
+if (height {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+} */
+
+/* const age = '18';
+if (age === 18) console.log('You just became an adult!(strict)');
+
+if (age == 18) console.log('You just became an adult!(loose)');
+
+const favorite = Number(prompt("What's your favorite number?"))
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) { // '23' === 23 => FALSE
+    console.log('Cool! 23 is an amazing number');
+} else if (favorite === 7) {
+    console.log('7 is also cool number!');
+} else if (favorite === 9) {
+    console.log('9 is also cool number!');
+}
+else (
+    console.log('Number is not 23 or 7 or 9')
+)
+
+if (favorite !== 23) console.log('Why not 23?'); */
