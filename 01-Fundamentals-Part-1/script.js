c1b0075106456d8e0f3a65114fdd5f6da039b1df
sgrazys <s.grazys@gmail.com>
2023-02-17 22:35:09 +0200

    Logical Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -238,3 +238,32 @@ if (favorite !== 23) console.log('Why not 23?'); */
 
 ////////////////////////////////////
 // Logical Operators
+
+/* 
+And - && (true when ALL conditions are true)
+or - || (true when at least condition si true)
+not - ! (Invert true/false value)
+*/
+
+/* const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense); */
+
+
+/* if (hasDriversLicense && hasGoodVision) {
+    console.log('Sarah is able to drive!')
+} else {
+    console.log('Someone else should drive...');
+} */
+
+/* const isTired = false // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!')
+} else {
+    console.log('Someone else should drive...');
+} */
