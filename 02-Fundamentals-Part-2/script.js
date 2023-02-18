c3338319945969182269a51b5dd55b8ecbd90804
sgrazys <s.grazys@gmail.com>
2023-02-18 17:46:01 +0200

    Arrow Function

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -35,9 +35,9 @@ const num = Number('23');
 console.log(num); */
 
 ////////////////////////////////////
-//FUNCTIONS DECLARATION vs. EXPRESSIONS
+//FUNCTIONS DECLARATION, EXPRESSIONS, ARROW FUNCTION
 
-//FUNCTION DECLARATIOM
+/* //FUNCTION DECLARATIOM
 function calcAge1(birthYear) {
   return 2037 - birthYear;
 
@@ -51,4 +51,20 @@ const calcAge2 = function (birthYear) {
 }
 
 const age2 = calcAge2(2000);
-console.log(age1, age2);
+console.log(age1, age2); */
+
+
+//ARROW FUNCTION
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(2000);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement}`
+}
+
+console.log(yearsUntilRetirement(2000, 'Saulius'));
+console.log(yearsUntilRetirement(1986, 'Algis'));
