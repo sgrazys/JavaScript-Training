ef73549e040143346b54b7fd755c47b4d165b35c
sgrazys <s.grazys@gmail.com>
2023-02-18 17:05:49 +0200

    Functions

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/* let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive!'); */
+
+// const private = 534;
+
+////////////////////////////////////
+//FUNCTIONS
+
+function logger() {
+  console.log('My name is Saulius');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+console.log(num);
