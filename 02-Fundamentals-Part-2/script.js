2ce5210529ec78504b526ad6c864a2f4583027d1
sgrazys <s.grazys@gmail.com>
2023-02-21 23:59:42 +0200

    the FOR loop. Intro

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -297,3 +297,21 @@ if (colleagues.includes('Morgan')) {
 
 // console.log(person.getBio());
 
+////////////////////////////////////
+// ITERATION: THE FOR LOOP
+
+// console.log('Lifting weights repetition 1 🏋️');
+// console.log('Lifting weights repetition 2 🏋️');
+// console.log('Lifting weights repetition 3 🏋️');
+// console.log('Lifting weights repetition 4 🏋️');
+// console.log('Lifting weights repetition 5 🏋️');
+// console.log('Lifting weights repetition 6 🏋️');
+// console.log('Lifting weights repetition 7 🏋️');
+// console.log('Lifting weights repetition 8 🏋️');
+// console.log('Lifting weights repetition 9 🏋️');
+// console.log('Lifting weights repetition 10 🏋️');
+
+// for loop keeps running while condition is TRUE
+for (let i = 1; i <= 30; i++) {
+  console.log(`Lifting weights repetition ${i} 🏋️`);
+}
