ccb4c84775273902354e160d23392655ddeb8aa2
sgrazys <s.grazys@gmail.com>
2023-02-23 01:00:38 +0200

    Challenge #4 my solution

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -408,16 +408,16 @@ if (colleagues.includes('Morgan')) {
 
 // WHILE LOOP
 
-let i = 1;
-while (i <= 10) {
-  // console.log(`WHILE: Lifting weights repetition ${i} 🏋️`);
-  i++
-}
+// let i = 1;
+// while (i <= 10) {
+//   // console.log(`WHILE: Lifting weights repetition ${i} 🏋️`);
+//   i++
+// }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
+// let dice = Math.trunc(Math.random() * 6) + 1;
 
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log(`Loop is about to end... You rolled ${dice}`);
-}
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log(`Loop is about to end... You rolled ${dice}`);
+// }
